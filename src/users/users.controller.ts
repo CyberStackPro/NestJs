153c7fbb41ca396e054ff65840e5a19f37dced00
CyberStackPro <yeabsragebriel@gmail.com>
2024-11-30 16:45:14 +0300

    First rest Api with nestjs

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -12,34 +12,44 @@ import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  constructor(private usersServices: UsersService) {}
+  constructor(private readonly usersServices: UsersService) {}
   @Get() // GET /users
   findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return this.usersServices.getUsers();
-  }
-
-  @Get('interns') // GET /users/interns
-  findAllInterns() {
-    return [];
+    return this.usersServices.findAll(role);
   }
 
   @Get(':id') // GET /users/:id
   findOne(@Param('id') id: string) {
-    return { id };
+    return this.usersServices.findOne(+id);
   }
 
   @Post() // POST /users
-  create(@Body() user: { user: any }) {
-    return user;
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersServices.create(user);
   }
 
   @Patch(':id') // PATCH /users/:id
-  updateOne(@Param('id') id: string, @Body() updateUser: any) {
-    return { id, ...updateUser };
+  updateOne(
+    @Param('id') id: string,
+    @Body()
+    updateUser: {
+      name: string;
+      email: string;
+      role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+    },
+  ) {
+    return this.usersServices.updateOne(+id, updateUser);
   }
 
   @Delete(':id') // GET /users/:id
   deleteOne(@Param('id') id: string) {
-    return { id };
+    return this.usersServices.deleteOne(+id);
   }
 }
